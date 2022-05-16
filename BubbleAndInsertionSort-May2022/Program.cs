f1f46e0a2a28ec5011e2d8cbc78a6ea29314c908
KacperPag <kacperpagorek@outlook.com>
2022-05-16 11:47:34 +0100

    Add project files.

---
+++ BubbleAndInsertionSort-May2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BubbleAndInsertionSort_May2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
